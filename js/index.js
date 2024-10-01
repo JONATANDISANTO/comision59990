f68e7b63174fcfe92bc7fefe6e14cf0d962687bf
jonatan di santo <tuestiloftx@gmail.com>
2024-10-01 07:28:10 -0300

    inicio proyecto javascript primera entrega

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const SUELDOBASICO = 803.000;
+const ANTIGUEDAD = 0.01;
+const PRESENTISMO = .0833;
+const APORTES = {
+    jubilacion: 0.11,
+    obraSocial: 0.03,
+    sindicato: 0.02
+};
+
+let añosAntiguedad = parseInt(prompt("ingresa antiguedad en cantidad de años"))
+if (isNaN(añosAntiguedad)) {
+    console.log("Por favor, ingresa un número valido."),
+} else {
+    console.log("años antiguedad ingresado: " + añosAntiguedad);
+}
+let diasTrabajados = 28
+
+
+
+
+
+
+let periodo = Septiembre/2024
+let categotia = vendedor
+let jornada = 48
