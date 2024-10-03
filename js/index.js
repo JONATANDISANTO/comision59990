b15548ebd9c04594d88964832320b7375c1bb7df
jonatan di santo <tuestiloftx@gmail.com>
2024-10-02 23:16:20 -0300

    primera entrega agregando ciclo for

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ el mismo pide que se ingrese, categoria, antiguedad, dias trabajados. el mismo c
 cobrará presentismo o no, tomando en cuenta que trabaja 6 dias a la semana y en el periodo 09/2024 hay 21 dias 
 de lunes a sabados, tambien se imprime en consola los siguientes valores: 
 
+0"elige la cantidad de empleados a calcular"(ciclo for)
 1"sueldo basico" 
 2"antiguedad laboral"
 3"calculo de cobro por antiguedad" 
@@ -25,64 +26,69 @@ const APORTES = {
 };
 let periodo = "Septiembre/2024";
 let diasDeTrabajo = 21;
-let seleccionaCategoria = prompt("Ingresa una categoría (vendedor, cocinero, administrativo, directivo)");
+let cantidadEmpleados = parseInt(prompt("¿Cuántos empleados deseas calcular?"), 10);
 
-let sueldoBasico;
+for (let i = 0; i < cantidadEmpleados; i++) {
+    let seleccionaCategoria = prompt("Ingresa una categoría (vendedor, cocinero, administrativo, directivo)");
 
-switch (seleccionaCategoria.toLowerCase()) { 
-  case "vendedor":
-    sueldoBasico = 750000;
-    break;
-  case "cocinero":
-    sueldoBasico = 850000;
-    break;
-  case "administrativo":
-    sueldoBasico = 780000;
-    break;
-  case "directivo":
-    sueldoBasico = 2000000;
-    break;
-  default:
-    alert("No seleccionaste una opción válida");
-}
+    let sueldoBasico;
 
-if (sueldoBasico) {
-  console.log("Tu Sueldo Basico es de:" + "$" + sueldoBasico);
-}
-
-let añosAntiguedad = parseInt(prompt("Ingresa antigüedad basada en años de 0 a 40"), 10);
-
-if (isNaN(añosAntiguedad)) {
-    console.log("Por favor, ingresa un número válido.");
-} else if (añosAntiguedad >= 41) {
-    console.log("Por favor, ingresa un número menor o igual a 40.");
-} else {
-    console.log("Años de antigüedad ingresados: " + añosAntiguedad);
-}
-
-let calculoAntiguedad = (añosAntiguedad * ANTIGUEDAD * sueldoBasico);
-console.log("Importe por Antigüedad:" + "$" + calculoAntiguedad);
-
-let diasTrabajados = parseInt(prompt("Ingresa los días que fuiste a trabajar"), 10);
-
-if (isNaN(diasTrabajados)) {
-    console.log("Por favor, ingresa un número válido para los días trabajados.");
-} else {
-    let sueldoBruto; 
-
-    if (diasTrabajados < 21) {
-        sueldoBruto = sueldoBasico + calculoAntiguedad; 
-        console.log("Este mes perdiste el presentismo.");
-    } else {
-        sueldoBruto = (PRESENTISMO * sueldoBasico) + calculoAntiguedad + sueldoBasico; 
-        console.log("Este mes no perdiste el presentismo. Se te aplicará un beneficio del " + (PRESENTISMO * 100) + "%.");
+    switch (seleccionaCategoria.toLowerCase()) { 
+      case "vendedor":
+        sueldoBasico = 750000;
+        break;
+      case "cocinero":
+        sueldoBasico = 850000;
+        break;
+      case "administrativo":
+        sueldoBasico = 780000;
+        break;
+      case "directivo":
+        sueldoBasico = 2000000;
+        break;
+      default:
+        alert("No seleccionaste una opción válida");
+        continue; 
     }
 
-    console.log("Tu Sueldo Bruto a Cobrar en periodo:" +  periodo + " es de:" + "$" + sueldoBruto);
+    if (sueldoBasico) {
+      console.log("Tu Sueldo Basico es de:" + "$" + sueldoBasico);
+    }
 
- 
-    const sueldoNeto = calcularSueldoNeto(sueldoBruto);
-    console.log("Total Sueldo Neto a Cobrar es:" + "$" + sueldoNeto);
+    let añosAntiguedad = parseInt(prompt("Ingresa antigüedad basada en años de 0 a 40"), 10);
+
+    if (isNaN(añosAntiguedad)) {
+        console.log("Por favor, ingresa un número válido.");
+    } else if (añosAntiguedad >= 41) {
+        console.log("Por favor, ingresa un número menor o igual a 40.");
+    } else {
+        console.log("Años de antigüedad ingresados: " + añosAntiguedad);
+    }
+
+    let calculoAntiguedad = (añosAntiguedad * ANTIGUEDAD * sueldoBasico);
+    console.log("Importe por Antigüedad:" + "$" + calculoAntiguedad);
+
+    let diasTrabajados = parseInt(prompt("Ingresa los días que fuiste a trabajar"), 10);
+
+    if (isNaN(diasTrabajados)) {
+        console.log("Por favor, ingresa un número válido para los días trabajados.");
+    } else {
+        let sueldoBruto; 
+
+        if (diasTrabajados < 21) {
+            sueldoBruto = sueldoBasico + calculoAntiguedad; 
+            console.log("Este mes perdiste el presentismo.");
+        } else {
+            sueldoBruto = (PRESENTISMO * sueldoBasico) + calculoAntiguedad + sueldoBasico; 
+            console.log("Este mes no perdiste el presentismo. Se te aplicará un beneficio del " + (PRESENTISMO * 100) + "%.");
+        }
+
+        console.log("Tu Sueldo Bruto a Cobrar en periodo:" +  periodo + " es de:" + "$" + sueldoBruto);
+
+
+        const sueldoNeto = calcularSueldoNeto(sueldoBruto);
+        console.log("Total Sueldo Neto a Cobrar en:" + "$" + sueldoNeto);
+    }
 }
 
 
